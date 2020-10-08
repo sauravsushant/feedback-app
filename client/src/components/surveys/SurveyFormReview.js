c27c0fc4790b6efe1657b1b20dc553b822a7ccfe
Saurav Kumar <saurav.sushant20@gmail.com>
2020-10-08 14:43:17 +0530

    Add files via upload

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,43 @@
+// SurveyFormReview Component - shows form inputs for review prior to submittal
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
+
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+  const reviewFields = _.map(formFields, ({ name, label }) => {
+    return (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h5>Please confirm your entries</h5>
+      {reviewFields}
+      <button className="btn yellow darken-3" onClick={onCancel}>
+        Back
+      </button>
+      <button
+        onClick={() => submitSurvey(formValues, history)}
+        className="btn green right"
+      >
+        Send Survey
+        <i className="material-icons right">email</i>
+      </button>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    formValues: state.form.surveyForm.values
+  };
+}
+
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: 'Survey Title',
+    name: 'title',
+    noValueError: 'You must provide a title'
+  },
+  {
+    label: 'Subject Line',
+    name: 'subject',
+    noValueError: 'You must provide a subject'
+  },
+  {
+    label: 'Email Body',
+    name: 'body',
+    noValueError: 'You must enter in a body for the email'
+  },
+  {
+    label: 'Recipient List',
+    name: 'recipients',
+    noValueError: 'You must enter in at least one recipient email address'
+  }
+];
